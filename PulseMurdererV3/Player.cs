3561e42e58aaf0e28dea166ccfadbef875a47867
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2025-05-14 09:43:12 +0200

    Add files via upload

---
+++ PulseMurdererV3/Player.cs
@@ -0,0 +1,87 @@
+﻿namespace PulseMurdererV3 {
+    public class Player {
+        private int id;
+        private string? name;
+        private string? avatar;
+        private bool isMurderer;
+        private bool hasVoted;
+        private bool isAlive;
+        private int votesRecieved;
+
+
+        public Player(int id, string? name, string? avatar, bool isMurderer, int votesRecieved) 
+        {
+            Id = id;
+            Name = name;
+            Avatar = avatar;
+            IsMurderer = isMurderer;
+            HasVoted = hasVoted;
+            IsAlive = isAlive;
+            VotesRecieved = votesRecieved;
+        }
+
+        public Player() {
+        }
+
+        public int Id {
+            get => id;
+            set{
+                id = value;
+            }
+        }
+
+        public string? Name {
+            get => name;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Name is null");
+                if (value.Length < 2)
+                    throw new ArgumentException("Name must be at least 4 characters long");
+                name = value;
+            }
+        }
+
+        public string? Avatar { 
+            get => avatar;
+            set{
+                avatar = value;
+            }
+        }
+
+        public bool IsMurderer { 
+            get => isMurderer;
+            set{
+                isMurderer = value;
+            }
+        }
+
+        public bool HasVoted{
+            get => hasVoted;
+            set{
+                hasVoted = value;
+            }
+        }
+
+        public bool IsAlive
+        {
+            get => isAlive;
+            set
+            {
+                isAlive = value;
+            }
+        }
+
+        public int VotesRecieved{
+            get => votesRecieved;
+            set{
+                votesRecieved = value;
+            }
+        }
+
+
+        public override string? ToString() {
+            return $"Id: {Id}, Name: {Name}, Avatar: {Avatar}, IsMurderer: {IsMurderer}";
+        }
+    }
+}
---
+++ PulseMurdererV3/PlayerRepository.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseMurdererV3
+{
+    public class PlayerRepository
+    {
+        private List<Player> players = new List<Player>();
+        private int _nextId = 1;
+
+        public PlayerRepository(){
+            this.AddPlayer(new Player() {Name = "Miki", IsMurderer = false, IsAlive = true});
+            this.AddPlayer(new Player() {Name = "Pap", IsMurderer = false, IsAlive = true});
+            this.AddPlayer(new Player() {Name = "Jais", IsMurderer = false, IsAlive = true});
+            this.AddPlayer(new Player() {Name = "Peter", IsMurderer = true, IsAlive = false});
+            this.AddPlayer(new Player() {Name = "Morten", IsMurderer = false, IsAlive = true});
+        }
+
+        public List<Player>? GetAllPlayers()
+        {
+            var playerList = new List<Player>(players);
+            return playerList;
+        }
+
+        public Player? AddPlayer(Player? player) {
+            if(player == null){
+                throw new ArgumentNullException("Player cannot be null");
+            }
+
+            player.Id = _nextId++;
+            players.Add(player);
+            return player;
+        }
+
+        public Player? GetPlayerById(int id){
+            int left = 0;
+            int right = players.Count - 1;
+
+            while(left <= right){
+                int mid = left + (right - left) / 2;
+
+                if(players[mid].Id == id){
+                    return players[mid];
+                }
+                else if(players[mid].Id < id){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+            return null;
+            //throw new ArgumentNullException("Player not found");
+        }
+
+        public Player? UpdatePlayer(int id, Player? newValues) {
+            Player? existingPlayer = this.GetPlayerById(id);
+            if(newValues == null){
+                throw new ArgumentNullException("New values cannot be null");
+            }
+            if(existingPlayer == null){
+                throw new ArgumentNullException("Player not found");
+            }
+
+            existingPlayer.Avatar = newValues.Avatar;
+            existingPlayer.IsMurderer = newValues.IsMurderer;
+            existingPlayer.HasVoted = newValues.HasVoted;
+            existingPlayer.IsAlive = newValues.IsAlive;
+            existingPlayer.VotesRecieved = newValues.VotesRecieved;
+            return existingPlayer;
+        }
+
+        public void ClearVotes(){
+            foreach(Player p in players){
+                p.HasVoted = false;
+                p.VotesRecieved = 0;
+            }
+        }
+
+        public void TallyVotes(){
+            Player? mostVoted = players.
+                Where(p => p.IsAlive).
+                OrderByDescending(p => p.VotesRecieved).
+                FirstOrDefault();
+            
+
+            if (mostVoted != null && mostVoted.IsAlive){
+                mostVoted.IsAlive = false;
+            }
+            ClearVotes();
+        }
+    }
+}
