9e469ea75656f502e88e05f5cbb20f9fd6348755
Miki-Aqqalu Kruse <mstachkruse@gmail.com>
2025-05-14 09:42:31 +0200

    Charles

---
+++ PulseMurdererREST/PulseMurdererREST/Program.cs
@@ -0,0 +1,42 @@
+using PulseMurdererV3;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<PlayerRepository>(new PlayerRepository());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
